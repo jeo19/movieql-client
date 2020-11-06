c5ad55ed5019c0b4d723d51bc3dfac5047b8c27a
jeo19 <terrakkk@outlook.com>
2020-11-06 22:23:28 +0800

    feat:Create the movie query with adding the 'gql' tag

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,17 @@
-import React from "react";
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+const GET_MOVIES = gql`
+    {
+        movies {
+            id
+            medium_cover_image
+        }
+    }
+`;
 
-export default () => <h1>Home</h1>;
+export default () => {
+    const { loading } = useQuery(GET_MOVIES);
+    console.log(loading);
+    return <h1>Home</h1>;
+};
